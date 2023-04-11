e66303a88d657d831b446dc57b3ec26fbe7902f4
Kenref <107287396+Kenref@users.noreply.github.com>
2023-04-11 13:24:45 +1000

    Added slider

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const largeBox = document.querySelector(".container")
+const sliderValue = document.querySelector("span")
+const inputSlider = document.querySelector("input")
+const boxes = [];
 
-let size = prompt("Pick a number from 1-100")
-size = Math.pow(size, 2)
+let size = 50
+draw(Math.pow(size, 2))
 
+inputSlider.oninput = (() => {
+    size = inputSlider.value;
+    sliderValue.textContent = size;
+    clear()
+    draw(Math.pow(size, 2))
+})
 
-for (i = 0; i < size; i++) {
-    let smallBox = document.createElement("div");
-    smallBox.classList.add("smallBox")
-    largeBox.appendChild(smallBox);
-
-    function mouseOn() {
-        smallBox.style.backgroundColor = "black"
-    }
-
-    
-    smallBox.addEventListener("mouseover", mouseOn)
+function clear() {
+    boxes.forEach(box => {
+        largeBox.removeChild(box);
+    });
+    boxes.length = 0;
 }
 
-largeBox.style.gridTemplateColumns = `repeat(${Math.sqrt(size)}, 1fr)`
+function draw(size) {
+    for (let i = 0; i < size; i++) {
+        let smallBox = document.createElement("div");
+        smallBox.classList.add("smallBox");
+        largeBox.appendChild(smallBox);
+        boxes.push(smallBox);
 
+        function mouseOn() {
+            this.style.backgroundColor = "black";
+        }
 
+        smallBox.addEventListener("mouseover", mouseOn);
+    }
 
-
-
+    largeBox.style.gridTemplateColumns = `repeat(${Math.sqrt(size)}, 1fr)`;
+}
